c19cde058d8a8640b6c3d5e2af02a294d2794888
Halyna Mazur <greenluck3.14159@gmail.com>
2024-06-25 19:35:49 +0300

    ch

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const API_URL_PROD = 'https://lit-garden-80835-f6f1bd11ce2a.herokuapp.com';
-export const API_URL = 'http://localhost:3001';
+export const API_URL = 'https://lit-garden-80835-f6f1bd11ce2a.herokuapp.com';
+export const API_URL_DEV = 'http://localhost:3001';
 export const APP_NAME = 'HoneydukeShop';
